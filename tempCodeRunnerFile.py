8458eb0526ca1f80788b9db97f7a35140317eff3
vicasi <vicasivex@gmail.com>
2022-07-07 14:19:49 -0300

    subindo numberguessing.v1

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,8 @@
-import random
-
-def modulo_do_jogo():
     number_to_be_found = random.randrange(0,15,1)
     loop = 1
     primeira_tentativa = int(input('chute um número entre 0 e 15: '))
     tentativas_restantes = 3
-    while loop < 2:
+    while loop < 3:
         if (primeira_tentativa == number_to_be_found):
             print('voce acertou! ')
             print('sobraram: ', tentativas_restantes, " tentativas_restantes")
@@ -14,7 +11,7 @@ def modulo_do_jogo():
             if tentativas_restantes == 0:
                     jogar_novamente = input('deseja jogar novamente?(s/n) ')
                     if jogar_novamente == 's':
-                        deseja_jogar = 's'
+                        guessingNumber()
                     elif jogar_novamente == 'n':
                         return
                     else:
@@ -24,18 +21,3 @@ def modulo_do_jogo():
             print('voce possui ', tentativas_restantes, " tentativas")
             print('a diferenca entre seu chute e a resposta e: ', hint)
             primeira_tentativa = int(input('tente novamente: '))
-
-
-def guessingNumber():
-    deseja_jogar = input('deseja jogar?(s/n)')
-    while deseja_jogar == 's':
-            modulo_do_jogo()
-        
-
-
-
-
-
-
-if __name__ == '__main__':
-    guessingNumber()
